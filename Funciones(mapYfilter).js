2c54a86c8a75acbfb6e0e963194f72e045e29196
NachoBerardo <nachoberardo2005@gmail.com>
2022-08-23 11:55:40 -0300

    ...

--- Funciones(mapYfilter).js
+++ Funciones(mapYfilter).js
@@ -11,3 +11,6 @@ console.log(OtroArray);
 const Miarray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let pares = Miarray2.filter((e) => e % 2 == 0);
 console.log(pares);
+
+let Miarray2 = [...Miarray, ...Miarray2];
+log(Miarray2);
